10fb9332d7dd36ed2e7c2ce1eccfc5ae8389314e
xobbba <xobbbag@mail.ru>
2022-09-15 19:32:51 +0500

    Skeleton

---
+++ src/models.py
@@ -0,0 +1,20 @@
+from pydantic import BaseModel
+
+
+class RegisterUserRequest(BaseModel):
+    name: str
+    surname: str
+    age: int
+
+
+class UserModel(BaseModel):
+    id: int
+    name: str
+    surname: str
+    age: int
+
+    class Config:
+        orm_mode = True
+
+
+
